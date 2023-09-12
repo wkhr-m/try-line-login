193ae81c42aaf2fa1a6bcda51072dac2263d1ff9
midoriwakahara <midoriwakahara@midoriwakaharanoMacBook-Pro.local>
2023-09-12 14:04:49 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+const onclick = () => {
+  const day = Date.now()
+  const clientId = "xxxxx"
+  const redirectUri = "http://localhost:5173"
+  window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${day}&scope=profile%20openid&nonce=09876xyz`
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <button @click="onclick">Lineでログイン</button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
